0e0c9d99024a1eef4f95ddbf1c0beb1ded68f693
davidlara089 <148598977+davidlara089@users.noreply.github.com>
2024-05-28 22:50:56 -0600

    holamundo.java

--- holamundo.java
+++ holamundo.java
@@ -1,5 +1,5 @@
-public class HolaMundo {
+public class holamundo {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("hola mundo");
     }
 }
