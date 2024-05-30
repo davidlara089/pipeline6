76cbda1c1d2f09536e75fded11daeeb99ce54ec4
davidlara089 <148598977+davidlara089@users.noreply.github.com>
2024-05-29 23:01:16 -0600

    Delete holamundo.java

--- holamundo.java
+++
@@ -1,9 +0,0 @@
-public class holamundo {
-    public static void main(String[] args) {
-        mostrarEnConsola();
-    }
-
-    public static void mostrarEnConsola() {
-        System.out.println("Hola Mundo");
-    }
-}
